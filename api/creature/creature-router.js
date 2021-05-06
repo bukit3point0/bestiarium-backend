5bdc6e4e6ebff3029e98abf92c8373907a660430
bukit3point0 <bukit3point0@gmail.com>
2021-05-06 17:08:54 -0400

    creature routes created

--- api/creature/creature-router.js
+++ api/creature/creature-router.js
@@ -10,18 +10,66 @@ const CreatureType = require('../creature-types/creatureType-model')
 const router = express.Router()
 
 // [GET] '/api/creatures/'
+router.get('/', logger, (req, res, next) => {
+    Creature.get(req.query)
+    .then(creatures => {
+        res.status(200).json(creatures)
+    })
+    .catch(next)
+})
 
 // [GET] '/api/creatures/id'
+router.get('/:id', logger, validateCreatureId, (req, res) => {
+    res.json(req.creature)
+})
 
 // [GET] '/api/creatures/:id/creatureTypes'
+router.get('/:id/creatureTypes', logger, validateCreatureId, (req, res, next) => {
+    Creature.getCreatureTypes(req.params.id)
+    .then(creatureTypes => {
+        res.status(200).json(creatureTypes)
+    })
+    .catch(next)
+})
 
 // [POST] '/api/creatures/'
+router.post('/', (req, res, next) => {
+    Creature.insert(req.body)
+    .then(creature => {
+        res.status(201).json(creature)
+    })
+    .catch(next)
+})
 
 // [POST] '/api/creatures/:id/creatureTypes'
+router.post('/', logger, validateCreatureName, validateCreatureId, (req, res, next) => {
+    const creatureId = {...req.body, creatureType_id: req.params.id}
+    Creature.insert(creatureId)
+    .then(creatureType => {
+        res.status(201).json(creatureType)
+    })
+    .catch(next)
+})
 
 // [PUT] '/api/creatures/:id'
+router.put('/:id', logger, validateCreatureId, (req, res, next) => {
+    Creature.update(req.params.id, req.body)
+    .then(creature => {
+        res.status(200).json(creature)
+    })
+    .catch(next)
+})
 
 // [DELETE] '/api/creatures/:id
+router.delete('/:id', logger, validateCreatureId, (req, res, next) => {
+    Creature.remove(req.params.id)
+    .then(() => {
+        res.status(200).json({
+            message: `creature deleted`
+        })
+    })
+    .catch(next)
+})
 
 
 module.exports = router
