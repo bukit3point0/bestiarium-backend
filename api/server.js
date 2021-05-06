89a9da3508e5e24ec6e3b09c48701bd8f7680e93
bukit3point0 <bukit3point0@gmail.com>
2021-05-06 16:35:49 -0400

    init commit, server.js created

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const path = require('path')
+
+const express = require('express')
+
+const creatureRouter = require('./creature/creature-router')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('/api/creatures', creatureRouter)
+
+server.use(express.static(path.join(__dirname, "client/build")))
+
+server.get('/api/*', (req,res) => {
+    res.json({
+        test: "confirmed"
+    })
+})
+
+server.use('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
